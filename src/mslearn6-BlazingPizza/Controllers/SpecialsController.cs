c90a97731048c5136c8a131d12f94c39a0b2baf3
Azrubael <i0638464000@gmail.com>
2026-08-07 20:51:04 +0300

    Updated 'src/mslearn6-BlazingPizza/'.

---
+++ src/mslearn6-BlazingPizza/Controllers/SpecialsController.cs
@@ -0,0 +1,28 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using BlazingPizza.Data;
+
+namespace BlazingPizza.Controllers;
+
+
+/// <summary>
+/// This class creates a controller that allows us to query the database
+/// for pizza specials and returns them as JSON at the URL
+/// </summary>
+[Route("specials")]
+[ApiController]
+public class SpecialsController : Controller
+{
+    private readonly PizzaStoreContext _db;
+
+    public SpecialsController(PizzaStoreContext db)
+    {
+        _db = db;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Model.PizzaSpecial>>> GetSpecials()
+    {
+        return (await _db.Specials.ToListAsync()).OrderByDescending(s => s.BasePrice).ToList();
+    }
+}
---
+++ src/mslearn6-BlazingPizza/Data/PizzaService.cs
@@ -0,0 +1,13 @@
+namespace BlazingPizza.Data;
+
+public class PizzaService
+{
+  public Task<Pizza[]> GetPizzasAsync()
+  {
+    // Call your data access technology here
+    return Task.FromResult(Array.Empty<Pizza>());
+
+  }
+
+
+}
