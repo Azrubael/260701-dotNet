5854812dbebc42388f34fdd2a95b71018b824c07
Azrubael <i0638464000@gmail.com>
2026-08-16 17:05:17 +0300

    The initial commit of the project 'src/260816-awards/' intended to calculate the monthly awards. Now it checks the selected directory and search defined files .xlsx

---
+++ src/260816-awards/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _260816_awards;
+
+using static DirectoryHelper;
+
+class Program
+{
+
+  static void Main()
+  {
+    string directoryPath = "d:/tmp/experiment/";
+    string?[]? filesList = FindFiles(directoryPath);
+    string[] xlsxFiles = [];
+    Dictionary<string, string> matchedFiles;
+
+    if (filesList == null || filesList.Length == 0)
+    {
+      Console.WriteLine($"Директорія {directoryPath} не містить жодного файлу, що підходить");
+      Environment.Exit(1);
+    }
+
+    filesList.ToList().ForEach(Console.WriteLine);
+    xlsxFiles = [.. filesList
+        .Where(f => f != null)    // Filter out null elements
+        .Select(f => f!)];
+    matchedFiles = CheckMatching(xlsxFiles, directoryPath);
+    string?[] dates = MakeArrayOfKeys(matchedFiles);
+
+  }
+
+
+}
