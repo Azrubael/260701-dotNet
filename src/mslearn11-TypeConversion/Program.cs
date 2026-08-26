a03288bf47adc4c1ebba3c0fd43f35c5181e70f8
Azrubael <i0638464000@gmail.com>
2026-08-26 10:28:57 +0300

    Added the project 'src/mslearn11-TypeConversion/*'.

---
+++ src/mslearn11-TypeConversion/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace mslearn11_TypeConversion;
+
+using System.Globalization;
+
+class Program
+{
+  static void Main()
+  {
+    CultureInfo.CurrentCulture = new CultureInfo("en-US");
+
+    int first = 2;
+    string second = "4";
+    string result = first + second;
+    Console.WriteLine(result);
+
+    Console.WriteLine("Hello, World!");
+  }
+}
+
+
